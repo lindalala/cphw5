da1bbf334e9d77f9013d41e2f5a699c0d2becd34
Linda Li <lxl@andrew.cmu.edu>
2015-04-21 05:15:08 -0400

    basic workings

---
+++ client/script.js
@@ -0,0 +1,126 @@
+var rocketx = 0;
+var rockety = 0;
+var rocketPos = 0;
+var globalRocketX;
+var globalRocketY;
+var globalRocketPos;
+var board;
+var offset;
+var moneybox;
+var startTime;
+var step;
+
+var socket = io.connect('http://localhost:8080');
+var moneyBox;
+
+socket.on('step', function (data) {
+  console.log("STEP");
+  globalRocketX = data.globalRocketX;
+  globalRocketY = data.globalRocketY;
+  globalRocketPos = data.globalRocketPos;
+  board = data.board;
+  offset = data.offset;
+  money = data.money;
+  startTime = data.startTime;
+  step = data.step;
+  redraw();
+});
+
+socket.on('start', function (data) {
+  console.log('START');
+  globalRocketX = data.globalRocketX;
+  globalRocketY = data.globalRocketY;
+  globalRocketPos = data.globalRocketPos;
+  board = data.board;
+  offset = data.offset;
+  money = data.money;
+  startTime = data.startTime;
+  step = data.step;
+  setup();
+});
+
+socket.on('gameover', function (data) {
+  gameOver(data.money, data.time);
+});
+
+function setup() {
+  var gameboard = $("#gameboard");
+  gameboard.empty();
+  var rocket = $("<div class='rocket'></div>");
+  var globalRocket = $("<div class='globalrocket'></div>");
+
+  $(gameboard).append(rocket);
+  $(gameboard).append(globalRocket);
+  for (var i=0; i<board.length-1; i++){
+    var gridRow = $("<div class='gridrow' id='row_"+i+"'></div>");
+    $(gameboard).append(gridRow);
+    for (var j=0; j<board[0].length; j++){
+      if (board[i][j]) {
+        $(gridRow).append($("<div class='gridcell enemy'></div>"));
+      } else {
+        $(gridRow).append($("<div class='gridcell free'></div>"));
+      }
+    }
+  }
+}
+
+function gameOver(money, time){
+  // clearInterval(interval);
+  // money = Math.min(2.000, money, Math.round(((0.001*(time-startTime))/1000)*1000)/1000);
+  // $('input[name="time"]').val(time/1000);
+  // $('input[name="earnings"]').val(money);
+  // $('#mturk_form').submit();
+  // alert('Your HIT has been submitted. \n You have earned: $' + money);
+  alert('game over');
+}
+
+function redraw() {
+  $(".globalrocket").css("left", globalRocketX + "px");
+
+  $("#moneybox").text("$" + money);
+
+  for (var i=0; i<board.length-1; i++){
+    var gridRow = $("#row_" + i);
+    gridRow.empty();
+    for (var j=0; j<board[0].length; j++){
+      var gridCell;
+      var iOff = (offset+i) % (board.length);
+      if (board[iOff][j]) {
+        gridCell = $("<div class='gridcell enemy'></div>");
+      } else {
+        gridCell = $("<div class='gridcell free'></div>");
+      }
+      $(gridRow).append(gridCell);
+    }
+  }
+  $(".enemy").each(function(i,el) {
+    $(el).css("background-color", "blue");
+  });
+}
+
+$(document).ready(function() {
+
+  $(document).keydown(function(e) {
+    if(e.keyCode==37) {
+      // left arrow clicked
+      rocketPos--;
+      rocketx -= step;
+    } else if(e.keyCode == 39) {
+      // right arrow clicked
+      rocketPos++;
+      rocketx += step;
+    }
+
+    if (rocketPos < 0){
+      rocketPos = board[0].length - 1;
+      rocketx += (board[0].length)*step;
+    } else if(rocketPos > board[0].length - 1) {
+      rocketPos = 0;
+      rocketx -= (board[0].length)*step;
+    }
+
+    $(".rocket").css("left", rocketx + "px");
+    socket.emit('vote', { 'rocketPos': rocketPos });
+  });
+
+});
---
+++ server.js
@@ -0,0 +1,119 @@
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('client'));
+var server = app.listen(8080);
+
+var globalRocketX;
+var globalRocketY;
+var globalRocketPos;
+var board;
+var offset;
+var boardLen;
+var money;
+var startTime;
+var step;
+
+var votes = {};
+var interval;
+var userCount = 0;
+
+var io = require('socket.io')(server);
+io.sockets.on('connect', function (socket) {
+  userCount++;
+
+  if (userCount === 1) {
+    globalRocketX = 0;
+    globalRocketY = 0;
+    globalRocketPos = 0;
+    board = [[0,0,0,0,0],
+             [0,0,0,0,1],
+             [0,0,0,0,0],
+             [0,0,0,0,0],
+             [0,1,0,1,1],
+             [0,0,0,0,1],
+             [0,0,0,0,1]];
+    offset = 0;
+    money = 0.000;
+    startTime = new Date().getTime();
+    step = 50;
+
+    interval = setInterval(emitStep, 1000);
+  }
+
+  io.sockets.emit('start', {
+    globalRocketX: globalRocketX,
+    globalRocketY: globalRocketY,
+    globalRocketPos: globalRocketPos,
+    board: board,
+    offset: offset,
+    money: money,
+    startTime: startTime,
+    step: step
+  });
+
+  socket.on('vote', function(data) {
+    votes[socket.id] = data.rocketPos;
+  });
+
+  socket.on('disconnect', function() {
+    userCount--;
+  });
+
+  // if (money === 2.000 || board[(offset+boardLen-1) % (board.length)][rocketPos]) {
+  //   gameOver(money, new Date().getTime());
+  //   return;
+  // }
+});
+
+function emitStep() {
+  updateGame();
+  console.log(offset);
+  io.sockets.emit('step', {
+    globalRocketX: globalRocketX,
+    globalRocketY: globalRocketY,
+    globalRocketPos: globalRocketPos,
+    board: board,
+    offset: offset,
+    money: money,
+    startTime: startTime,
+    step: step
+  });
+}
+
+function updateGame() {
+  console.log(offset);
+  offset--;
+  if (offset < 0) {
+    offset = board.length - 1;
+  }
+
+  updateGlobalRocket();
+}
+
+function updateGlobalRocket() {
+  var votesCount = {};
+  var maxCount = 0;
+  var modeVote = null;
+
+  for (key in votes) {
+    vote = votes[key];
+    if (votesCount[vote]) {
+      votesCount[vote]++;
+    } else {
+      votesCount[vote] = 1;
+    }
+
+    if (votesCount[vote] > maxCount) {
+      var modeVote = vote;
+      maxCount = votesCount[vote];
+    }
+  }
+
+  if (modeVote !== null) {
+    globalRocketPos = modeVote;
+    globalRocketX = modeVote*step;
+  }
+  votes = {};
+}
